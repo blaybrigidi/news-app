3112e71f4d4f56631e069bec2f8273db2080d7b9
blaybrigidi <135924669+blaybrigidi@users.noreply.github.com>
2025-03-22 10:13:13 +0000

    changes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
-const path = require('path');
-const { createRequire } = require('module');
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
